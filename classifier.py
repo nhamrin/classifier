0cb5387b3d35e47cc19b246dc5972d5841ce3681
nhamrin <104506300+nhamrin@users.noreply.github.com>
2025-03-09 21:06:10 +0100

    Update to include feature selection
    Update to include feature selection RFE and increase the n_estimators to 650. Also add updated predictions file.

--- classifier.py
+++ classifier.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
-#from sklearn.ensemble import GradientBoostingClassifier
 from xgboost import XGBClassifier
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import RFE
 
 search_params = False
 test_model = False
@@ -21,7 +21,7 @@ y_train = data[y_column]
 
 categorical_columns = ["x7", "x12"]
 
-preprosessor = ColumnTransformer([
+preprocessor = ColumnTransformer([
     ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output=False), categorical_columns)
 ], remainder = 'passthrough')
 
@@ -32,15 +32,20 @@ if test_model:
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.3, random_state=42)
 
     if search_params:
+        classifier = XGBClassifier(random_state=42)
+        rfe = RFE(estimator=classifier)
+
         model = Pipeline([
-            ('preprosessor', preprosessor),
-            ('classifier', XGBClassifier(random_state=42))
+            ('preprocessor', preprocessor),
+            ('rfe', rfe),
+            ('classifier', classifier)
         ])
 
         param_grid = {
+            'rfe__n_features_to_select': [4, 5, 6, 7],
             'classifier__n_estimators': [400, 500, 600, 700],
             'classifier__learning_rate': [0.01, 0.05, 0.1, 0.2],
-            'classifier__max_depth': [2, 3, 4] 
+            'classifier__max_depth': [2, 3, 4]
         }
 
         grid_search = GridSearchCV(model, param_grid, cv=3, scoring='accuracy', n_jobs=-1)
@@ -52,22 +57,29 @@ if test_model:
             accuracy = accuracy_score(y_test, y_pred)
             print(f"Best model accuracy: {accuracy:.4f}")
             print(f"Best parameters: {grid_search.best_params_}")
+            #Best parameters calculated to be:
+            #n_features to select = 5
+            #n_estimators = 500 (manually tuned to 650)
+            #learning _rate = 0.05
+            #max_depth = 2
         else:
             print("Grid search did not complete successfully")
     else:
         model = Pipeline([
-            ('preprosessor', preprosessor),
-            ('classifier', XGBClassifier(learning_rate=0.05, max_depth=2, n_estimators=600, random_state=42))
+            ('preprocessor', preprocessor),
+            ('rfe', RFE(estimator=XGBClassifier(learning_rate=0.05, max_depth=2, n_estimators=650, random_state=42), n_features_to_select=5))
         ])
 
         model.fit(x_train, y_train)
+
         y_pred = model.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
         print(f"Model accuracy: {accuracy:.4f}")
+        print(f"The chosen features are: {model.get_feature_names_out()}")
 else:
     model = Pipeline([
-        ('preprosessor', preprosessor),
-        ('classifier', XGBClassifier(learning_rate=0.05, max_depth=2, n_estimators=600, random_state=42))
+        ('preprocessor', preprocessor),
+        ('rfe', RFE(estimator=XGBClassifier(learning_rate=0.05, max_depth=2, n_estimators=650, random_state=42), n_features_to_select=5))
     ])
 
     model.fit(x_train, y_train)
